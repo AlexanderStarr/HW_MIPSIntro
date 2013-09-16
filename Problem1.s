f6a707d06914b4d51a6a48e9ee1f39ceed55c76b
Alexander Starr <AlexanderLStarr@gmail.com>
2013-09-16 15:25:13 -0500

    Updated comments for problems 1 and 2.

--- Problem1.s
+++ Problem1.s
@@ -3,9 +3,9 @@ myName:  .asciiz    "Alexander Starr"
          .globl     main
          .text
 main:
-         li         $v0, 4       # System call code for Print String
+         li         $v0, 4       # System call code for print_string
          la         $a0, myName  # load address of myName into $a0
          syscall                 # Print the myName message
 
-         li         $v0, 10      # System call code for terminate 
-         syscall                 # return control to system
+         li         $v0, 10      # System call code for exit 
+         syscall                 # Return control to system
--- Problem2.s
+++ Problem2.s
@@ -6,15 +6,17 @@ nline:   .asciiz    "\n"
 main:   
          la         $t0, array    # Load address of first element
          lw         $t1, 0($t0)   # Load first element
-loop:    beq        $t1, $zero, exit 
+loop:    beq        $t1, $zero, exit
+                                  # Jumps to label 'exit' if element = 0
          li         $v0, 11       # System call code for print_char
-         move       $a0, $t1
+         move       $a0, $t1      # Pass current element as an argument
          syscall                  # Print the current character
          li         $v0, 4        # System call code for print string
          la         $a0, nline    # Load address of newline character
-         syscall
+         syscall                  # Print newline
          addi       $t0, $t0, 4   # Advance the address
          lw         $t1, 0($t0)   # Load the new element
          j          loop          # Repeat the loop
-exit:    li         $v0, 10       # System call code for terminate 
-         syscall                  # return control to system
+
+exit:    li         $v0, 10       # System call code for exit 
+         syscall                  # Return control to system
